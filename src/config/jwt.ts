3517d15ec8f986d9601337e6649084b64c71f492
Andres Minota <andresnotami@gmail.com>
2024-04-04 00:03:56 -0500

    Agrega configuración JWT y middleware de autenticación

---
+++ src/config/jwt.ts
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken';
+import { envs } from './envs';
+
+const JWT_SECRET = envs.JWT_SECRET;
+
+export class JwtAdapter {
+  static generateToken(payload: Object, expiresIn: string = '2h'): Promise<string | null> {
+    return new Promise((resolve) => {
+      jwt.sign(payload, JWT_SECRET, { expiresIn }, (err, token) => {
+        if (err) {
+          return resolve(null);
+        }
+        resolve(token!);
+      });
+    });
+  }
+  static validateToken<T>(token: string): Promise<T | null> {
+    return new Promise((resolve) => {
+      jwt.verify(token, JWT_SECRET, (err, decoded) => {
+        if (err) {
+          return resolve(null);
+        }
+        resolve(decoded as T);
+      });
+    });
+  }
+}
--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
 import { AuthRepository, CustomError, RegisterUserDto } from '../../domain';
+import { JwtAdapter } from '../../config';
+import { UserModel } from '../../data/mongodb';
 
 export class AuthController {
 
@@ -21,12 +23,24 @@ export class AuthController {
     const [error, registerUserDto] = RegisterUserDto.create(req.body);
     if (error) return res.status(400).json({ error });
     this.authRepository.register(registerUserDto!)
-      .then((user) => res.status(200).json(user))
+      .then(async (user) => {
+        res.json({
+          user,
+          token: await JwtAdapter.generateToken({ id: user.id })
+        });
+      })
       .catch((error) => this.handleError(error, res));
   }
 
   logingUser = (req: Request, res: Response) => {
     res.json('Login route Controller');
+  }
+  getUSers = (req: Request, res: Response) => {
+    UserModel.find().then((users) => {
+      res.json({
+        token: req.body.user
+      });
+    }).catch((error) => this.handleError(error, res));
   }
 
 }
